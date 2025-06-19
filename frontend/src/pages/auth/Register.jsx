21d608fab2a515c757a4a70e5d3344d8e6234ef6
Damien Cuvillier <165381045+Damien-Cuvillier@users.noreply.github.com>
2025-06-19 12:01:36 +0200

    Update Register.jsx

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -131,7 +131,7 @@ const Register = () => {
                 name="entreprise"
                 type="text"
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Entreprise (optionnel)"
+                placeholder="Entreprise"
                 value={formData.entreprise}
                 onChange={handleChange}
               />
